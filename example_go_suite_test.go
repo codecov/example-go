bed8995d0e4d2325419ccf5dd7955155c40a5887
stevepeak <steve@stevepeak.net>
2014-09-22 08:43:16 -0700

    chack

--- example_go_suite_test.go
+++ example_go_suite_test.go
@@ -10,6 +10,5 @@ import (
 
 func TestAwesome(t *testing.T) {
     RegisterFailHandler(Fail)
-    junitReporter := reporters.NewJUnitReporter(os.Getenv("CI_REPORT"))
     RunSpecsWithDefaultAndCustomReporters(t, "Awesome Suite", []Reporter{junitReporter})
 }
