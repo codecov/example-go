4fd0f2788cf2f3109ce1a4eaa24b2b72206e335a
Bo-Yi Wu <appleboy.tw@gmail.com>
2016-07-13 23:41:30 +0800

    Replace find with go list command.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- go.test.sh
+++ go.test.sh
@@ -3,12 +3,10 @@
 set -e
 echo "" > coverage.txt
 
-for d in $(find . -maxdepth 10 -type d); do
-    if ls $d/*.go &> /dev/null; then
-        go test -coverprofile=profile.out -covermode=atomic $d
-        if [ -f profile.out ]; then
-            cat profile.out >> coverage.txt
-            rm profile.out
-        fi
+for d in $(go list ./... | grep -v vendor); do
+    go test -coverprofile=profile.out -covermode=atomic $d
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
     fi
 done
