7a36b58c471c289cc40ec105089569563a30e9aa
Tom Hu <tomhu1096@gmail.com>
2021-01-29 17:21:34 -0500

    Cover Subtract function

--- calculator_test.go
+++ calculator_test.go
@@ -4,6 +4,12 @@ import "testing"
 
 func TestAdd(t *testing.T) {
 	if got, want := Add(1, 2), 3; got != want {
-		t.Errorf("add method produced wrong result. expected: %d, got: %d", want, got)
+		t.Errorf("Add method produced wrong result. expected: %d, got: %d", want, got)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	if got, want := Subtract(3, 2), 1; got != want {
+		t.Errorf("Subtract method produced wrong result. expected: %d, got: %d", want, got)
 	}
 }
