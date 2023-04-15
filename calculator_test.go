4f768bbcfa298383ab358a179d38332a28ad1b50
DeepakBomjan <44976635+DeepakBomjan@users.noreply.github.com>
2023-04-15 07:23:32 +0545

    Update calculator_test.go

--- calculator_test.go
+++ calculator_test.go
@@ -72,25 +72,25 @@ func TestMultiply(t *testing.T) {
 	}
 }
 
-func TestDivide(t *testing.T) {
-	tables := []struct {
-		x int
-		y int
-		n float64
-		e string
-	}{
-		{1, 2, 0.5, ""},
-		{1.0, 2.0, 0.5, ""},
-		{0, 2.0, 0, ""},
-		{-4, 2.0, -2.0, ""},
-		// {2.0, 0, 0, "Cannot divide by 0"},
-	}
+// func TestDivide(t *testing.T) {
+// 	tables := []struct {
+// 		x int
+// 		y int
+// 		n float64
+// 		e string
+// 	}{
+// 		{1, 2, 0.5, ""},
+// 		{1.0, 2.0, 0.5, ""},
+// 		{0, 2.0, 0, ""},
+// 		{-4, 2.0, -2.0, ""},
+// 		// {2.0, 0, 0, "Cannot divide by 0"},
+// 	}
 
-	for _, table := range tables {
-		if total, err := Divide(table.x, table.y); err != nil && err.Error() != table.e {
-			t.Errorf("Divide of (%d/%d) produced wrong error, got %v, want %v.", table.x, table.y, err.Error(), table.e)
-		} else if total != table.n {
-			t.Errorf("Divide of (%d/%d) produced wrong result, got %f, want %f.", table.x, table.y, total, table.n)
-		}
-	}
-}
+// 	for _, table := range tables {
+// 		if total, err := Divide(table.x, table.y); err != nil && err.Error() != table.e {
+// 			t.Errorf("Divide of (%d/%d) produced wrong error, got %v, want %v.", table.x, table.y, err.Error(), table.e)
+// 		} else if total != table.n {
+// 			t.Errorf("Divide of (%d/%d) produced wrong result, got %f, want %f.", table.x, table.y, total, table.n)
+// 		}
+// 	}
+// }
